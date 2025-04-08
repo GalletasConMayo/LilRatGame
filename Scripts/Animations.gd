f1af62d81219ebb256725982383cb7eff70cf6e9
GalletasConMayo <m.galaz.p.1998@gmail.com>
2025-04-08 08:37:48 -0400

    fix project errors

--- Scripts/Animations.gd
+++ Scripts/Animations.gd
@@ -4,15 +4,15 @@ func _ready():
 	global_variables.state_signal.connect(play_animations)
 
 func play_animations(current_state)->void:
-<<<<<<< HEAD
+
 	#if current_state == "cwall":
 		#$AnimationPlayer.play("normal_hit_2")
 	#else:
 	$AnimationPlayer.play(current_state)
-=======
+
 	if current_state == "cwall":
 		#$AnimationPlayer.play("normal_hit_2")
 		$AnimationPlayer.play("wall")
 	else:
 		$AnimationPlayer.play(current_state)
->>>>>>> 9deac1bc914cf78ad10a00b4ef3a9e9324bc9a3a
+
